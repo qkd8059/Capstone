54abef986ae88abefd96c7fea25cc7782ea6252f
Jing (Jenny) Zhang <44584108+qkd8059@users.noreply.github.com>
2020-11-25 22:01:27 -0500

    Update robust_cvar_cost.py

--- Optimization/robust_cvar_cost.py
+++ Optimization/robust_cvar_cost.py
@@ -17,7 +17,7 @@ def robust_cvar_cost(mu,Q, card, price_table, date, old_weight, old_ticker, targ
     """
     # Define parameters used for Monte Carlo simulation
     ## the number of simulated asset price paths
-    num_paths = 1000
+    num_paths = 100
     ## get current prices of the current date
     current_prices = price_table[date]
     ## the time length of simulation in the unit of estimation of mu and Q. Here T represents rebalancing every 8 weeks
