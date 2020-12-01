f2d96757febd2e7315d5dd4ca464ee9d67068c3b
Jinxuan (Tony) Zhang <40329370+zhan4053@users.noreply.github.com>
2020-12-01 16:45:52 -0500

    Updated files for front-end interface

--- web/sign-up/form-validation.js
+++ web/sign-up/form-validation.js
@@ -10,6 +10,12 @@
     Array.prototype.filter.call(forms, function (form) {
       form.addEventListener('submit', function (event) {
         if (form.checkValidity() === false) {
+          alert("Please fill in all required fields.")
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        else{
+          window.location.href = "../sign-in/sign-in.html";
           event.preventDefault()
           event.stopPropagation()
         }
@@ -19,3 +25,21 @@
     })
   }, false)
 })()
+
+function validatePassword(confirm_password){
+  if ($("#password").val() != confirm_password.value){
+    confirm_password.setCustomValidity("Password don't match")
+  }
+  else{
+    confirm_password.setCustomValidity("")
+  }
+}
+
+function showPassword() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
