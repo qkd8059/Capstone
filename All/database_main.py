82697db77c558ff45668145b2a984ab6447ebd45
Xinyue Zhang <33614023+sherryz7120@users.noreply.github.com>
2020-12-01 22:12:44 -0500

    Update database_main.py

--- All/database_main.py
+++ All/database_main.py
@@ -30,7 +30,7 @@ class Database(object):
     def get_date():
         #set the time interval for getting the stock price
         dtFromDate = '2005-09-30'
-        dtToDate = '2020-09-30' #str(dt.datetime.now().date())
+        dtToDate =  str(dt.datetime.now().date())
         dates = [dtFromDate, dtToDate]
         return dates
     
@@ -93,12 +93,6 @@ class Database(object):
         data_dict = data.to_dict("record")
         collection.insert_many(data_dict)
     
-    def write_time_series (data,lookback,card,risk_appetite):
-        #save 4*4*3 cumulated returns into database
-        collection = 'timeseries_L'+str(lookback)+'_C'+str(card)+'_R'+str(risk_appetite)
-        print(collection)
-        Database.save_into_mongo(data,collection)
-    
     def clean_col_save (data, collection):
         client = pymongo.MongoClient("mongodb+srv://admin:admin@mie479.mvqsq.mongodb.net/MIE479?retryWrites=true&w=majority") 
         db = client.MIE479
@@ -110,7 +104,22 @@ class Database(object):
             collection.delete_many({})
         data.reset_index(inplace=True)
         data_dict = data.to_dict("record")
-        collection.insert_many(data_dict)        
+        collection.insert_many(data_dict)  
+        
+    def write_time_series (data,lookback,card,risk_appetite):
+        #save 4*4*3 cumulated returns into database
+        collection = 'timeseries_L'+str(lookback)+'_C'+str(card)+'_R'+str(risk_appetite)
+        print(collection)
+        Database.clean_col_save(data,collection)
+    
+    def get_timeseries_name (data,lookback,card,risk_appetite):
+        name = 'timeseries_L'+str(lookback)+'_C'+str(card)+'_R'+str(risk_appetite)
+        return name
+    
+    def write_portfolio (data, userid, portfid):
+        collection = 'portfolio_'+userid+'_'+str(portfid)
+        print(collection)
+        Database.clean_col_save(data,collection)
         
         
     
