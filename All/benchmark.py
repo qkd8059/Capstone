182ca5fce7e051199e2ef113267cb7d6a17e1c13
Xinyue Zhang <33614023+sherryz7120@users.noreply.github.com>
2020-12-01 20:38:26 -0500

    Update benchmark.py

--- All/benchmark.py
+++ All/benchmark.py
@@ -5,20 +5,51 @@ from database_main import Database
 from regime_test import *
 
 spy = Database.read_data('SPY')
-print(spy)
 time = spy['Date']
-#time = time[1:]
-
+print(len(time))
 
 price = spy['Adj Close']
-ret = []
+price = pd.DataFrame(price)
 
-for i in range(1,len(price)):
-    ret.append((price[i]-price[i-1])/price[i-1])
+ret = price.pct_change().dropna()
+ret = ret['Adj Close'].values.tolist()
 
-print(len(time))
-print(len(ret))
+#save dates
+for i in range(9,int(len(time)/4)):
+    date.append(time[i*4])
+date = pd.DataFrame(date, columns = ['Date'])  
+Database.clean_col_save(date,'timeseries_dates')
 
-cum_ret = Regime_test.cum_return(ret)
-print(len(cum_ret))
-plt.plot(time,cum_ret)
+#save SPY cumulated return
+monthly_ret = []
+date = []
+for i in range(0,186):
+    monthly_ret.append(ret[i*4]) 
+
+cum_ret = Regime_test.cum_return(monthly_ret)
+plt.plot(date,cum_ret[:-1])
+cum_ret = pd.DataFrame(cum_ret, columns = ['cum ret'])
+
+Database.clean_col_save(cum_ret,'SPY_ret')
+
+#save 60/40 fund cumulated return
+fund = Database.read_data('6040fund')
+time = fund['Date']
+
+price = fund['Adj Close']
+price = pd.DataFrame(price)
+
+ret = price.pct_change().dropna()
+ret = ret['Adj Close'].values.tolist()
+
+monthly_ret = []
+date = []
+for i in range(0,186):
+    monthly_ret.append(ret[i*4]) 
+
+cum_ret = Regime_test.cum_return(monthly_ret)
+plt.plot(date,cum_ret[:-1])
+cum_ret = pd.DataFrame(cum_ret, columns = ['cum ret'])
+
+Database.clean_col_save(cum_ret,'6040fund_ret')
+
